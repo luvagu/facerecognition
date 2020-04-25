c8df562f40f58159d59e4fd04642862dc9763089
luiavag <luiavag@gmail.com>
2020-04-25 13:27:25 -0500

    Updates

---
+++ precache-manifest.04e214a0ce977549c95ca9fa1ca63d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ba1bdb1ddfeecdd81f30e7a37efa8c",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "1f47b967c4846e9b7900",
+    "url": "/facerecognition/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "950e4de92bf1cf3b6f22",
+    "url": "/facerecognition/static/css/main.b3abd401.chunk.css"
+  },
+  {
+    "revision": "1f47b967c4846e9b7900",
+    "url": "/facerecognition/static/js/2.04f79e2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.04f79e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950e4de92bf1cf3b6f22",
+    "url": "/facerecognition/static/js/main.cc371812.chunk.js"
+  },
+  {
+    "revision": "b1c0e8aee0d06b10d4f1",
+    "url": "/facerecognition/static/js/runtime-main.b5d4f63f.js"
+  }
+]);
